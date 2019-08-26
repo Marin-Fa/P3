6d79d566f88bf7279ac3443c2fe6d489508a9012
Marin <marin.fayolle@gmail.com>
2019-08-26 14:21:22 +0200

    ...

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 $(() => {
   const newMap = new Map("map", 45.758061, 4.83363, 13);
-<<<<<<< HEAD
   // const newCanvas = new Canvas("canvas");
   // const newBooking = new Booking("booking", newMap);
-=======
-  const newBooking = new Booking("booking", newMap);
->>>>>>> master
 });
