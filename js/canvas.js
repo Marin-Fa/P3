3ffddb4c86eec6012242e482f1d1e36c4ccb9e43
Marin <marin.fayolle@gmail.com>
2019-09-13 11:07:20 +0200

    JQurey last update

--- js/canvas.js
+++ js/canvas.js
@@ -10,9 +10,6 @@ class Canvas {
     this.ctx.strokeStyle = "#b20000";
     this.initPointerEvents();
 
-    // document
-    //   .getElementById("clear_btn_canvas")
-    //   .addEventListener("click", this.clearPad.bind(this));
     $("#clear_btn_canvas").on("click", this.clearPad());
   }
   initPointerEvents() {
